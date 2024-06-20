1b4254d5358b4690bf4f873fa324f256aa069977
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-20 06:05:34 +0000

    main conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let anyNumber = 89
 let stringNumber = String(anyNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1= "hello"
+let str2 = " Vishal"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);    //these are bad practice of code
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
