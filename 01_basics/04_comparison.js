2e551ce728d13feaa780c18c392b29184812aa6e
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-20 06:17:29 +0000

    done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -11,8 +11,7 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
-
-console.log(undefined == 0);
+console.log(undefined == 0); //avoid these kindaa conversions
 console.log(undefined > 0);
 console.log(undefined < 0);
 
