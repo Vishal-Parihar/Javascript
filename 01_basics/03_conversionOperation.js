5c0cb99dc68b58d8acf82916af8c83a1118b4f26
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-20 05:39:44 +0000

    datatypesConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = 60
+let score = "60xyz"
+//console.log(typeof score);
+
+let valueInNmber = Number(score)
+//console.log(typeof valueInNmber);
+//console.log(valueInNmber);
+
+//conversions
+// "33" => 33
+// "33xyz" =>NaN
+// true/false => 1/0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1/0 =>true/false
+// "" =>false
+// "Vishal" => true
+
+let anyNumber = 89
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
