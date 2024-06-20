c49daa0f15efe102d9668ae924482a23152aa7d8
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-20 05:11:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"  // treats all JS code as a newer version
+
+//alert(3+3)  //we are using nodjs not browser (it will work in browser not in IDE)
+
+console.log(3+
+    3  );       //wrong, code readibility should be high
+
+    let name = "Vishal"       
+    let age = 20              
+    let isLoggedIn = true     
+                               
+//string
+//int ,bigInt
+//boolean
+//null =>standalone value
+//undefined
+//symbol =>unique
+
+//object
+
+console.log(typeof age);
