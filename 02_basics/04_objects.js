755ebf630310d8d327630dadb68253895fe6ff4b
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-22 06:50:04 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,20 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email :"user@gmail.com",
+    fullName : {
+        userFullName :{
+            firstName : "Vishal",
+            lastName : "Parihar"
+        }
+    }
+
+}
+console.log(regularUser);
