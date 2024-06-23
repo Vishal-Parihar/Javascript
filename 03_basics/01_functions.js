f440b2559e83ff8855887687f7bb54776bb61df0
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-23 16:00:23 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,42 @@ function loginUserMessage(userName) {
     return `${userName} just logged in `
 }
 // console.log(loginUserMessage("Vishal"))
-console.log(loginUserMessage());           // o/p - undefined just logged in
+// console.log(loginUserMessage());           // o/p - undefined just logged in
+
+
+// function calculateCartPrice(number1){
+//     return number1
+//     }
+//     console.log(calculateCartPrice(500)); 
+
+function calculateCartPrice(...number1){               //(...  = rest and spread operator)
+    return number1
+}
+// console.log(calculateCartPrice(100,200,300,400));      // returns array of numbers used in cart updation in ecommerce websites
+
+
+function calculateCartPrice(val1,val2,...number2){               //(...  = rest and spread operator)
+    return number2
+}
+// console.log(calculateCartPrice(10,20,30,40));     // here 10,20 are assigned to val1 and val2 and rest of the values are printed in array
+
+const user = {
+    userName :"Vishal",
+    userId :"0021"
+}
+
+function objectHandle(anyobject){
+    console.log(`Username is ${anyobject.userName} and userId is ${anyobject.userId}`);
+}
+// objectHandle(user)
+objectHandle({                                     //obj can be defined here also
+    userName : "Mohan",
+    userId :123
+})
+
+const myNewArray = [100,200,300,400,500]
+function getArrValue(getArray){
+    return getArray[2]                     //returns second value of array
+}
+// console.log(getArrValue(myNewArray));\
+console.log(getArrValue([1,2,3,4,5]));
