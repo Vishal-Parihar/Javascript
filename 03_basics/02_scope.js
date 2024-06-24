dadd00ab95450d7496fb2ccee5a3539bbc5c54d2
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-24 06:48:15 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -13,3 +13,46 @@ if (true) {                               //block scope - their values can only 
 // console.log(a);       //global value of a will be printed ie 300
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hitesh"                  //bigger fn cannot access values of sub-fn inside it but smaller fn can
+                                               //access values of parent fn
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);                //error out of scope
+
+     two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);                      //error
+}
+
+// console.log(username);                        //error
+
+
+//[Function use case-1]
+function addOne(num){                 
+    return num+1
+} 
+console.log(addOne(5))
+
+
+//[Function use case-2]              
+const addTwo = function(number){                //this is sometime also called as an expression
+    return number + 2 
+}
+
+console.log(addTwo(5))
