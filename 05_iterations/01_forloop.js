165a5d97b4f460ed2a12917da380bbf786adf805
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-26 06:21:41 +0000

    iterations

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,48 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for(let i=0; i<=10; i++){
+    const element = i
+   if (element == 5 ) {
+    // console.log("midway reached");
+   }
+//    console.log(element);
+}
+
+
+// for (let i= 0; i <=10; i++) {
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//        console.log(i + '*'+ j + '='+  i * j);
+        
+//     }
+// }
+
+
+
+// let myArray = ["Superman", "batman", "spiderman"]
+// for (let index = 0; index <myArray.length; index++) {
+//     const element =myArray[index];
+//     console.log(element);
+    
+// }
+
+
+
+//break and continue
+//break = stops the code when the particular condition is met
+//continue = skips the current situation/condition and runs the rest of the code
+
+for (let index = 0; index <=20; index++) {
+   if (index == 5) {
+    console.log(`5 detected`);
+    // break
+    continue
+     }
+    console.log(index);
+}
+
+
+
---
+++ 05_iterations/02_dowhile.js
@@ -0,0 +1,23 @@
+//WHILE LOOP
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value :${index}`);
+    index = index + 2
+    }
+
+    let myArray = ["superman","batman", "spiderman"]
+    let arr =0
+    while (arr<myArray.length) {
+        console.log(myArray[arr]);
+        arr++
+        
+    }
+
+    //DO-WHILE LOOP
+
+    let score = 0
+    do {
+        console.log(`score is : ${score}`);
+        score++
+    } while (score <=10);
