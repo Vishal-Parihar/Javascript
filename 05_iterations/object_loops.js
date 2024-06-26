02f554f831951c2b37ea6fa5bb74b9f2bb5feb20
Vishal-Parihar <147621166+Vishal-Parihar@users.noreply.github.com>
2024-06-26 07:43:56 +0000

    obj loops

---
+++ 05_iterations/object_loops.js
@@ -0,0 +1,19 @@
+const myObject ={
+    js : "javascript",
+    rb : "ruby",
+    py : "python",
+    ts : "typesctipt"
+}
+for (const key in myObject) {
+    // console.log(`${key} is for - ${myObject[key]}`);
+   
+}
+
+
+//but when we use for-in loop in arrays we get keys as o/p (0,1,2,3) instead of actual values
+const languages = ["javascript","py","rb","ts"]
+
+for (const key in languages) {
+    // console.log(key);          //returns keys (0,1,2,3)
+    console.log(languages[key]);
+  } 
