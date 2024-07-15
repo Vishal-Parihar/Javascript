f07ecc3cc420a80ac775efb7d140a06040621021
Vishal-Parihar <vishalparihar1008@gmail.com>
2024-07-15 14:35:47 +0530

    pr

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -50,13 +50,65 @@ const promiseFour = new Promise(function(resolve,reject){
 })
 
 //consume promiseFour
-promiseFour.then( (user)=>{          //user is a parameter in fn
+promiseFour
+.then( (user)=>{          //user is a parameter in fn
      console.log(user);
      return user.userName
-}).then( (username)=> {                        //chaining , value from first then will be transferred here
+})
+.then( (username)=> {                        //chaining , value from first then will be transferred here
 console.log(username);
-}).catch(function(error){
+})
+.catch(function(error){
     console.log(error);
 })
+.finally(()=>console.log('The promise is either resolved or rejected'))
 
 
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false          //checking for true condition
+        if(!error){
+            resolve({userName:"Javascript",password:"001" })
+        }
+        else{
+            reject('ERROR: SOMETHING NOT FOUND')
+        }
+    },1000)
+})
+
+//we can conusme promise using async - await also
+//The keyword async before a function makes the function return a promise
+//The await keyword can only be used inside an async function.
+//The await keyword makes the function pause the execution and wait for a resolved promise before it continues
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive     //recieves data
+        console.log(response);         
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+//function to get user from a api
+// async function getUser(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/Vishal-Parihar')
+//         const data = await response.json()             //converts the string values recived to json
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E :",error);
+        
+//     }
+// }
+// getUser()
+
+//same above function using '.then'
+fetch('https://api.github.com/users/Vishal-Parihar')
+.then((response) =>{
+    return response.json()
+})
+.then((data)=>{           //to handle the above data a second '.then' is used
+    console.log(data);
+})
+.catch((error)=>console.log(error))
